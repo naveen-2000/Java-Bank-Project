b363d83883d2cde455117113ea48823cf5d325d3
naveen-2000 <58387418+naveen-2000@users.noreply.github.com>
2020-07-29 11:13:26 +0530

    Update JavaBankProject.java

--- JavaBankProject.java
+++ JavaBankProject.java
@@ -10,17 +10,17 @@ public class Bank{
 			System.out.println("Please Enter a Menu Option");
 			System.out.println("(1): Add Customer to Bank");
 			System.out.println("(2): Change Customer Name");
-			System.out.println("(3): Check Account Balence");
-			System.out.println("(4): Modify Account Balence");
+			System.out.println("(3): Check Account Balance");
+			System.out.println("(4): Modify Account Balance");
 			System.out.println("(5): Summary of All Accounts");
 			System.out.println("(-1): Quit");
 			
 			int userInput = input.nextInt();
 			if(userInput == 1){
 				System.out.println("Bank Add Customer Menu");
-				System.out.println("Please Enter an account balence");
-				double balence = input.nextDouble();
-				accountBalences[size] = balence;
+				System.out.println("Please Enter an account balance");
+				double balance = input.nextDouble();
+				accountBalances[size] = balance;
 				System.out.println("Please enter an account holder name: ");
 				input.nextLine();
 				String name = input.nextLine();
